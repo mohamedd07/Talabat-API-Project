dee22449422fe30a642d7c083fdea5ed09f25a69
Mohamed <moham@MOHAMEDZZ>
2024-04-11 05:03:58 +0200

    Add project files.

---
+++ Talabat.API/Program.cs
@@ -0,0 +1,36 @@
+namespace Talabat.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                // To Document the all APIs as UI with all details to make end-user(Front-End Dev) to consume api
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
